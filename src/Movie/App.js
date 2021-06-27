154da224717489e1291f752e68c9ca866f16a771
ahsor <jemicom_it@naver.com>
2021-06-27 11:46:01 +0900

    Movie App.js Input Add

--- src/Movie/App.js
+++ src/Movie/App.js
@@ -5,50 +5,43 @@ import Movie from './Movie';
 function App() {
   const movies = [
     {id:1, title:'아라비카', year:2001},
-    {id:2, title:'콜롬비아', year:2001},
-    {id:3, title:'에티오피아', year:2001},
-    {id:4, title:'아프리카', year:2001},
+    {id:2, title:'콜롬비아', year:2006},
+    {id:3, title:'에티오피아', year:2010},
+    {id:4, title:'아프리카', year:2011},
   ]
+
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+
   const renderMovie = movies.map(movie=>{
     return(
-      <Movie />
+      <Movie movie={movie} key={movie.id}/>
     )
   })
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    console.log(movieTitle, movieYear)
+  }
   return(
     <div className="App">
+      <form onSubmit={onSubmit}>
+        <input type="text"
+               placeholder="영화제목"
+               onChange={e=>setMovieTitle(e.target.value)} 
+               value={movieTitle}
+        /> <br />
+        
+        <input type="text"
+               placeholder="개봉년도"
+               onChange={e=>setMovieYear(e.target.value)} 
+               value={movieYear}
+        /> <br />
+        <button type="submit">영화 추가</button>
+      </form>
       { renderMovie }
-    </div>
-  )
-}
-/*
-function App() {
-  const array = [1,2,3];
-  const obj = {name:'kim', age:30};
-  const node = <h1>제목노드</h1>;
-  const func = ()=>{ console.log('메시지')}
 
-  return(
-    <div className="App">
-      <h1> 컴포넌트 비교 </h1>
-        <ClassChild
-          str={"kim"}
-          boolValue={true}
-          numValue={1}
-          arrayValue={array}
-          objValue={obj}
-          nodeValue={node}
-          funcValue={func}
-      />
-      
-      <ClassFunction str={"park"}
-          boolValue={true}
-          numValue={1}
-          arrayValue={array}
-          objValue={obj}
-          nodeValue={node}
-          funcValue={func}/>
     </div>
   )
 }
-*/
+
 export default App;
